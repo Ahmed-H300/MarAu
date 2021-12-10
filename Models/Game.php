a76b7efff5c7705d29fccc5f1ebb0dc707fab38a
Mahmed amer <67114727+Mahmedamer@users.noreply.github.com>
2021-12-10 21:22:01 +0200

    Game Model

---
+++ Models/Game.php
@@ -0,0 +1,36 @@
+<?php
+
+class Game 
+{
+    public int $GameId;
+    public string $Name;
+    public string $Description;
+    // Icon ,Image and Video are stored With the GameId
+    public DateTime $ReleaseDate;
+    public float $Price;
+    public string $Type;
+    public string $Version;
+    public float $Sale;
+
+    public int $RatingCount;
+    public float $Rating;
+
+    public int $SellerId; 
+    public string $SellerName;
+
+    public int $FirstOwner;
+    public string $FirstOwnerName;
+
+    public string $OperatingSystem;
+    public string $MinimumCPU;
+    public string $RecommendedCPU;
+    public string $MinimumGPU;
+    public string $RecommendedGPU;
+    public string $MinimumRAM;
+    public string $RecommendedRAM;
+    public string $Storage;
+
+    public int $NumberOfOrders;
+    public DateTime $LastOrderDate;
+
+}
