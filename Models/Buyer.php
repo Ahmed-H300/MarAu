1da5056c691b58577b77d3c4d1c04797c09e7010
Ahmed-H300 <ahmed300hany@gmail.com>
2021-12-12 21:04:13 +0200

    Buyer Model

---
+++ Models/Buyer.php
@@ -0,0 +1,14 @@
+<?php
+
+class Buyer
+{
+    public int $Buyerid;
+    public int $AccountId;
+    public string $country;
+    // name ??
+    public string $Buyername;
+    
+    public int $Strikes;
+    public int $Balance;
+    public int $GamesCount;
+}
