8fc2c2d3d7a4b333e38e85fd4bbab7ddb76a759d
Mohammed Abdulhady <m.abdulhady00@gmail.com>
2021-12-12 16:11:10 +0200

    Seller Details

---
+++ Models/Seller.php
@@ -0,0 +1,12 @@
+<?php
+class Seller {
+    public int $SellerId;
+    public int $AccountId;
+    public string $SellerName;
+    public String $Country;
+    public String $ContactEmail;
+    public int $Strikes;
+    public int $Balance;
+    public int $GamesCount;
+    public float $AvgGamesRating;
+}
